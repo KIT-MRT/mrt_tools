6cb77221d1120ed96cd2b996f70322fea0dfedf0
Claudio Bandera <claudio.bandera@kit.edu>
2015-09-23 13:36:37 +0200

    Removed old folders

--- mrt_py_tools/mrt_py_tools/settings.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-# Default settings
-default_token_path = "~/.mrtgitlab/.token"
-default_ssh_path = "~/.ssh"
-default_repo_cache = "~/.mrtgitlab/repo_cache"
-default_repo_cache_time = 60  # in seconds
-default_host = "https://gitlab.mrt.uni-karlsruhe.de"
