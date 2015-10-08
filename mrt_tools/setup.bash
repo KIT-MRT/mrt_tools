e5937e126c93f859eb8cd66ffadcb81e47f35045
Claudio Bandera <claudio.bandera@kit.edu>
2015-10-08 12:27:33 +0200

    Removed unnecessary stuff

--- mrt_tools/setup.bash
+++ mrt_tools/setup.bash
@@ -1,8 +1,1 @@
-_MRT_COMPLETE=source mrt
-for command in $(ls mrt_tools/commands/*.py); do
-    cmd=$(basename $command)
-    cmd="${cmd%.*}"
-    if [ ! "$cmd" == "__init__" ]; then
-        alias mrt_$cmd="mrt $cmd"
-    fi
-done
+eval "$(_MRT_COMPLETE=source mrt)"
