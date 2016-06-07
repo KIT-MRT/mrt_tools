b6de0026f3886ebd2c67aa4c4922c1b1a21d1293
Claudio Bandera <claudio.bandera@kit.edu>
2016-06-07 16:29:39 +0200

    Added tests for credential manager

--- mrt_tools/tests/test_credentials.py
+++ mrt_tools/tests/test_credentials.py
@@ -1,8 +1,70 @@
-def test_import_gnome_keyring():
-    import keyring
-    keyring.set_keyring(keyring.backends.Gnome.Keyring())
-    keyring.get_password("test", "test")
+from mrt_tools.CredentialManager import *
+
+USER = "pytest_user"
+PASSW = "pytest_pw"
+TOKEN = "pytest_token"
+
+
+def test_base_credential_manager():
+    cm = BaseCredentialManager()
+    cm.store(USER, USER)
+    cm.store(PASSW, PASSW)
+    cm.store(TOKEN, TOKEN)
+
+    assert cm.get(USER) == ""
+    assert cm.get(PASSW) == ""
+    assert cm.get(TOKEN) == ""
+
+    cm.delete(USER)
+    cm.delete(PASSW)
+    cm.delete(TOKEN)
+
+    assert cm.get(USER) == ""
+    assert cm.get(PASSW) == ""
+    assert cm.get(TOKEN) == ""
+
+
+def test_gnome_credential_manager():
+    # cm = GnomeCredentialManager()
+    # cm.store(USER, USER)
+    # cm.store(PASSW, PASSW)
+    # cm.store(TOKEN, TOKEN)
+    #
+    # assert cm.get(USER) == USER
+    # assert cm.get(PASSW) == PASSW
+    # assert cm.get(TOKEN) == TOKEN
+    #
+    # cm.delete(USER)
+    # cm.delete(PASSW)
+    # cm.delete(TOKEN)
+    #
+    # assert cm.get(USER) is None
+    # assert cm.get(PASSW) is None
+    # assert cm.get(TOKEN) is None
+    # TODO This is not working yet
+    pass
+
+
+def test_file_credential_manager():
+    cm = FileCredentialManager()
+    cm.store(USER, USER)
+    cm.store(PASSW, PASSW)
+    cm.store(TOKEN, TOKEN)
+
+    assert cm.get(USER) == USER
+    assert cm.get(PASSW) == PASSW
+    assert cm.get(TOKEN) == TOKEN
+
+    cm.delete(USER)
+    cm.delete(PASSW)
+    cm.delete(TOKEN)
+
+    assert cm.get(USER) is None
+    assert cm.get(PASSW) is None
+    assert cm.get(TOKEN) is None
 
 
 if __name__ == '__main__':
-    test_import_gnome_keyring()
+    test_base_credential_manager()
+    test_file_credential_manager()
+    test_gnome_credential_manager()
