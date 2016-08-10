e21fc999566c683d70bc3043cb64c0f2aac6526c
Claudio Bandera <claudio.bandera@kit.edu>
2016-08-10 12:59:33 +0200

    Adjusted node wrapper template

--- mrt_tools/templates/node_base/class_name_node.cpp
+++ mrt_tools/templates/node_base/class_name_node.cpp
@@ -4,7 +4,7 @@ int main(int argc, char* argv[]) {
 
     ros::init(argc, argv, "${class_name}_node");
 
-    ${pkgname}::${ClassName} converter(ros::NodeHandle(), ros::NodeHandle("~"));
+    ${pkgname}::${ClassName} ${class_name}(ros::NodeHandle(), ros::NodeHandle("~"));
 
     ros::spin();
     return 0;
